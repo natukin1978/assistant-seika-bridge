8652cd8e3c5fdb74162c15372256fee2c4151d56
natukin1978 <natukin1978@hotmail.com>
2025-01-18 16:15:38 +0900

    The configuration file is now loaded from the current directory.

--- assistant_seika_bridge.py
+++ assistant_seika_bridge.py
@@ -1,15 +1,11 @@
 import asyncio
 import logging
+import os
+import sys
 
 import global_value as g
 from config_helper import readConfig
 from web_server_logic import start_web_server
-
-g.config = readConfig()
-
-# ロガーの設定
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 async def main():
@@ -17,4 +13,8 @@ async def main():
 
 
 if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
+    g.config = readConfig()
+    # ロガーの設定
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
--- config_helper.py
+++ config_helper.py
@@ -3,7 +3,5 @@ import os
 
 
 def readConfig(name: str = "config.json"):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    json_file_path = os.path.join(current_dir, name)
-    with open(json_file_path, "r", encoding="utf-8") as f:
+    with open(name, "r", encoding="utf-8") as f:
         return json.load(f)
