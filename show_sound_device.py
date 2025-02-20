e4d5055fd2a1a3cdbd3c419ce9c66104813eacd4
natukin1978 <natukin1978@hotmail.com>
2025-02-20 16:56:08 +0900

    It has been modified so that you can input the number and output the device name.

--- show_sound_device.py
+++ show_sound_device.py
@@ -1,13 +1,16 @@
 import sounddevice as sd
 
-
-def list_devices():
+if __name__ == "__main__":
     """利用可能なオーディオデバイスをリスト表示する"""
     devices = sd.query_devices()
     print(devices)
-
-
-if __name__ == "__main__":
-    list_devices()
-    print("以上")
-    input()
+    while True:
+        print("Enter the number and it will print the device name.", end=" ")
+        i_str = input()
+        if not i_str:
+            break
+        i = int(i_str)
+        device = devices[i]
+        hostapi = sd.query_hostapis(device["hostapi"])
+        full_name = f'{device["name"]}, {hostapi["name"]}'
+        print('"' + full_name + '"')
